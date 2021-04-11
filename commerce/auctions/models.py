8b4492480e2b6d3a9d637b499184dfd882419e8b
Sami Kasasbeh <Sami.Kasasbehdev@gmail.com>
2021-04-11 20:13:24 -0230

    Adding some initial models for lists bids comments

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Lists(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+
+    def __str__(self):
+        return f'{self.title}: Starting price {self.price} '
+
+class Bids(models.Model):
+    pass
+
+class Comments(models.Model):
+    pass
+
